4a0f6c6eba12e83d99b1f9e1d116468d8d4e0958
Tuigut <kipngetichb306@gmail.com>
2023-07-13 13:23:16 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,17 +4,17 @@
 t can be compiled and run on
  * return 0 (success)
  */
-int main void
+int main(void)
 {
 char a;
 int b;
 long int c;
 long long int d;
 float e;
-printf("size of char :%lu bytes (s)\n",(unsigned long)sizeof (a));
-printf("size of int :%lu bytes (s)\n",(unsigned long)sizeof (b));
-printf("size of long int :%lu bytes (s)\n",(unsigned long)sizeof (c);
-printf("size of long long int :%lu bytes (s)\n",(unsigned long)sizeof (d);
-printf("size of float :%lu bytes (s)\n",(unsigned long)size of (e));
+printf("size of char :%lu bytes (s)\n", (unsigned long)sizeof(a));
+printf("size of int :%lu bytes (s)\n", (unsigned long)sizeof(b));
+printf("size of long int :%lu bytes (s)\n", (unsigned long)sizeof(c);
+printf("size of long long int :%lu bytes (s)\n", (unsigned long)sizeof(d);
+printf("size of float :%lu bytes (s)\n", (unsigned long)size of(e));
 return (0);
 }
