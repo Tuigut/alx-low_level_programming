6af447ece3964e65e6f67524fef1fe8d85b5c8f0
Tuigut <kipngetichb306@gmail.com>
2023-07-13 12:58:59 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,8 +4,8 @@
  * main -program that prints a line followed by a standard error
  * return 1 (success)
  */
-int main (void)
+int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
