1f35dbfb56df461a5feb16ad6a5b21a3e56fcc64
Tuigut <kipngetichb306@gmail.com>
2023-07-17 05:36:37 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints the alphabet.
  *
- * Description: Prints the alphabet in uppercase
- *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char alp;
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
 
-	for (alp = 'A'; alp <= 'Z'; alp++)
-		putchar(alp);
-
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
 	putchar('\n');
-
 	return (0);
 }
 	
